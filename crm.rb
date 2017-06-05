bb2c9364d50215aad23adab93b57fd4fc0a8ed0e
vrunda2712 <angy.v27@gmail.com>
2017-06-05 15:14:59 -0400

    use of DELETE to delete contact

--- crm.rb
+++ crm.rb
@@ -18,6 +18,7 @@ get '/contacts/new' do
   erb :new_contact
 end
 
+# Create new contact
 post '/contacts'  do
   # puts params
   Contact.create(
@@ -29,24 +30,23 @@ post '/contacts'  do
    redirect to('/contacts')
 end
 
-get '/contacts/:id' do
-  @contact = Contact.find(params[:id].to_i)
-  if @contact
-    erb :contact
-  else
-    raise Sinatra::NotFound
-  end
-end
+# Find contact by ID
+# get '/contacts/:id' do
+#   @contact = Contact.find(params[:id].to_i)
+#   if @contact
+#     erb :contact
+#   else
+#     raise Sinatra::NotFound
+#   end
+# end
 
+# Edit Contact
 get '/contacts/:id/edit' do
   @contact = Contact.find(params[:id].to_i)
-  if @contact
-    erb :edit_contact
-  else
-    raise Sinatra::NotFound
-  end
+  erb :edit_contact
 end
 
+# Find contact by ID to edit
 put '/contacts/:id' do
   @contact = Contact.find(params[:id].to_i)
   if @contact
@@ -60,6 +60,22 @@ put '/contacts/:id' do
    else
      raise Sinatra::NotFound
   end
+end
+
+# Delete Contact
+get '/contacts/:id/delete' do
+  @contact = Contact.find(params[:id].to_i)
+  @contact.delete
+
+  redirect to('/contacts')
+end
+
+# Find contact by ID to delete
+delete '/contacts/:id' do
+  @contact = Contact.find(params[:id].to.i)
+  @contact.delete
+
+  redirect to('/contacts')
 end
 
 get '/about' do
